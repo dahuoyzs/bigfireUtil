2306cd9f8bb6914a487e84b4e434297554adad17
大火yzs <835476090@qq.com>
2019-08-21 23:48:48 +0800

    IPUtil  IP地址相关操作
    后续IP相关操作会继续更新

---
+++ bigfireUtil/IPUtil.java
@@ -0,0 +1,55 @@
+public class IPUtil {
+    //判断字符串是否为ip地址
+    public static boolean isIP(String str) {
+        // 如果长度不符合条件 返回false
+        if (str.length() < 7 || str.length() > 15) return false;
+        String[] arr = str.split("\\.");
+        //如果拆分结果不是4个字串 返回false
+        if (arr.length != 4) return false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < arr[i].length(); j++) {
+                char temp = arr[i].charAt(j);
+                //如果某个字符不是数字就返回false
+                if (!(temp > '0' && temp < '9')) return false;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            int temp = Integer.parseInt(arr[i]);
+            //如果某个数字不是0到255之间的数 就返回false
+            if (temp < 0 || temp > 255) return false;
+        }
+        return true;
+    }
+    //判断端口
+    public static Boolean isPort(String str) {
+        try {
+            Integer port = Integer.parseInt(str);
+            if (port > 0 && port < 65535) {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //获取自己的公网IP
+    public static String getIp() {
+        String ipUrl = "https://www.ip.cn/";
+        String resStr = HttpUtil.get(ipUrl);
+        String fromIp = StrUtil.subBetween(resStr, "您现在的 IP：<code>", "</code></p><p>所在地理位置");
+        return fromIp;
+    }
+
+    //获取当前ip的大概地址
+    public static String getAddress(String fromIp) {
+        String ipToAddrUrl = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query="+fromIp+"&resource_id=6006";
+        String res = HttpUtil.get(ipToAddrUrl);
+        String address = JSONObject.parseObject(res)
+                .getJSONArray("data")
+                .getJSONObject(0)
+                .getString("location");
+        return address;
+    }
+
+}
